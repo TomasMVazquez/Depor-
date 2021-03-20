cfd477e4a735bcc2d711efe62618a3ecce906f1c
Toms <tomas.manuel.vazquez@gmail.com>
2021-03-20 18:47:25 +0100

    Recycler View Sports (close #10)

--- app/src/main/java/com/applications/toms/depormas/data/model/Sport.kt
+++ app/src/main/java/com/applications/toms/depormas/data/model/Sport.kt
@@ -10,12 +10,13 @@ data class Sport(
     val id: Int?,
     val name: String?,
     val img: String?,
-    val max_players: Int?
+    val max_players: Int?,
+    var choosen: Boolean = false
 ): Parcelable{
 
     constructor() : this(null,null,null,null)
 
-    fun getDrawableInt(context: Context): Int = context.resources.getIdentifier(name,img,context.packageName)
+    fun getDrawableInt(context: Context): Int = context.resources.getIdentifier(img,"drawable",context.packageName)
 
 }
 
--- app/src/main/java/com/applications/toms/depormas/ui/screens/home/HomeFragment.kt
+++ app/src/main/java/com/applications/toms/depormas/ui/screens/home/HomeFragment.kt
@@ -3,6 +3,7 @@ package com.applications.toms.depormas.ui.screens.home
 import android.os.Bundle
 import android.util.Log
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
@@ -15,6 +16,8 @@ import com.applications.toms.depormas.data.repository.SportRepository
 import com.applications.toms.depormas.data.source.Network
 import com.applications.toms.depormas.data.source.database.RoomDataSource
 import com.applications.toms.depormas.data.source.database.SportDatabase
+import com.applications.toms.depormas.ui.adapters.SportAdapter
+import com.applications.toms.depormas.ui.adapters.SportListener
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.InternalCoroutinesApi
 
@@ -40,8 +43,15 @@ class HomeFragment : Fragment() {
         homeViewModel = ViewModelProvider(this,HomeViewModelFactory(sportRepository))
             .get(HomeViewModel::class.java)
 
+        val sportAdapter = SportAdapter(SportListener { sport ->
+            sport.choosen = !sport.choosen
+            Toast.makeText(requireContext(),"${sport.name} + ${sport.choosen}",Toast.LENGTH_SHORT).show()
+        })
+
+        binding.sportRecycler.adapter = sportAdapter
+
         homeViewModel.sports.observe(viewLifecycleOwner){
-            Log.d(TAG, "onCreateView: $it")
+            sportAdapter.submitList(it)
         }
 
         return binding.root
