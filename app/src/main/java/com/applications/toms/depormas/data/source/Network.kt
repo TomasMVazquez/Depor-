279228a005a3b1dc7104cb799283cb15ffe046f7
Toms <tomas.manuel.vazquez@gmail.com>
2021-03-28 11:03:35 +0200

    Add splash bkg and attributions

--- app/src/main/java/com/applications/toms/depormas/data/source/Network.kt
+++ app/src/main/java/com/applications/toms/depormas/data/source/Network.kt
@@ -1,22 +1,7 @@
 package com.applications.toms.depormas.data.source
 
-import android.util.Log
-import androidx.lifecycle.asLiveData
-import com.applications.toms.depormas.data.model.Sport
-import com.google.firebase.firestore.CollectionReference
-import com.google.firebase.firestore.FirebaseFirestoreException
-import com.google.firebase.firestore.QuerySnapshot
 import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.NonCancellable.cancel
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.channels.awaitClose
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.callbackFlow
-import kotlinx.coroutines.tasks.await
-import java.lang.Exception
 
 class Network: RemoteDataSource {
 
--- app/src/main/java/com/applications/toms/depormas/data/source/database/RoomDataSource.kt
+++ app/src/main/java/com/applications/toms/depormas/data/source/database/RoomDataSource.kt
@@ -3,11 +3,8 @@ package com.applications.toms.depormas.data.source.database
 import android.content.Context
 import com.applications.toms.depormas.data.model.Sport
 import com.applications.toms.depormas.data.model.asDatabaseModel
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.withContext
-import kotlin.coroutines.coroutineContext
 
 class RoomDataSource(context: Context): LocalDataSource {
 
