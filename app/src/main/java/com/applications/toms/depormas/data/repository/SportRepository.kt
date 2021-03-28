f024b928d4b2e9dd5ad6f501d83318adc4f3b738
Toms <tomas.manuel.vazquez@gmail.com>
2021-03-28 10:22:04 +0200

    Fix error of Firestore fetching data

--- app/src/main/java/com/applications/toms/depormas/data/repository/SportRepository.kt
+++ app/src/main/java/com/applications/toms/depormas/data/repository/SportRepository.kt
@@ -3,21 +3,14 @@ package com.applications.toms.depormas.data.repository
 import com.applications.toms.depormas.data.model.Sport
 import com.applications.toms.depormas.data.source.RemoteDataSource
 import com.applications.toms.depormas.data.source.database.LocalDataSource
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import java.util.concurrent.TimeUnit
+import kotlinx.coroutines.launch
 
 class SportRepository(
+        private val coroutineScope: CoroutineScope,
         private val localDataSource: LocalDataSource,
         private val remoteDataSource: RemoteDataSource) {
-
-    /*suspend fun getSports(): List<Sport> {
-        if(localDataSource.isEmpty()){
-            val sports = remoteDataSource.getSports()
-            localDataSource.saveSports(sports)
-        }
-        return localDataSource.getAllSports()
-    }*/
 
     fun getSports(): Flow<List<Sport>> {
         refreshSports()
@@ -25,8 +18,17 @@ class SportRepository(
     }
 
     private fun refreshSports(){
-        val sports = remoteDataSource.getSports()
-        localDataSource.saveSports(sports)
+        remoteDataSource.getSportsCollection().addSnapshotListener { value, error ->
+            if (error != null) {
+                return@addSnapshotListener
+            }
+            val list: MutableList<Sport>? = value?.toObjects(Sport::class.java)
+            list?.toList()?.let { listToSave ->
+                coroutineScope.launch {
+                    localDataSource.saveSports(listToSave)
+                }
+            }
+        }
     }
 
     companion object{
