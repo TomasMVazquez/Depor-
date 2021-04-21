7df22cc80405ac896b1907aab862286122597a7f
Toms <tomas.manuel.vazquez@gmail.com>
2021-04-21 19:25:39 +0200

    Changes to local DB

--- app/src/main/java/com/applications/toms/depormas/data/database/local/event/Event.kt
+++ app/src/main/java/com/applications/toms/depormas/data/database/local/event/Event.kt
@@ -1,17 +1,44 @@
-package com.applications.toms.depormas.data.database.remote
+package com.applications.toms.depormas.data.database.local.event
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.TypeConverters
+import com.applications.toms.depormas.data.database.Converters
 import com.applications.toms.depormas.domain.Location
 import com.applications.toms.depormas.domain.Sport
+import com.applications.toms.depormas.domain.Event as DomainEvent
 import java.sql.Timestamp
+import java.util.*
 
+@Entity(tableName = "event_table")
+@TypeConverters(Converters::class)
 data class Event(
-        val id: String?,
+        @PrimaryKey
+        val id: String,
         val event_name: String,
         val date: String,
         val time: String,
         val sport: Sport,
         val max_players: Int,
         val location: Location,
-        val created_date: Timestamp = Timestamp(System.currentTimeMillis()),
+        val created_date: Date = Date(System.currentTimeMillis()),
         var participants: Int = 0
 )
+
+fun Event.toDomainModel(): DomainEvent = DomainEvent(id, event_name, date, time, sport, max_players, location, created_date, participants)
+
+fun List<Event>.toDomainModel(): List<DomainEvent> {
+        return map {
+                DomainEvent(
+                        id = it.id,
+                        event_name = it.event_name,
+                        date = it.date,
+                        time = it.time,
+                        sport = it.sport,
+                        max_players = it.max_players,
+                        location = it.location,
+                        created_date = it.created_date,
+                        participants = it.participants
+                )
+        }
+}
--- app/src/main/java/com/applications/toms/depormas/data/database/local/event/EventDatabase.kt
+++ app/src/main/java/com/applications/toms/depormas/data/database/local/event/EventDatabase.kt
@@ -1,4 +1,35 @@
 package com.applications.toms.depormas.data.database.local.event
 
-class EventDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.applications.toms.depormas.R
+
+@Database(entities = [Event::class],version = 1,exportSchema = false)
+abstract class EventDatabase: RoomDatabase() {
+    abstract val eventDao: EventDao
+
+    companion object{
+        @Volatile
+        private var INSTANCE: EventDatabase? = null
+
+        fun getInstance(context: Context): EventDatabase {
+            synchronized(this){
+                var instance = INSTANCE
+                if (instance == null){
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        EventDatabase::class.java,
+                        context.getString(R.string.database_table_event)
+                    )
+                        .allowMainThreadQueries()
+                        .fallbackToDestructiveMigration()
+                        .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
 }
